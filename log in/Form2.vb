8d207281caa0e89ebb519c4a26896f7927f71df4
Shanker Busshetty <shankerbussetty@gmail.com>
2025-05-19 01:12:39 +0530

    Update Form2.vb

--- log in/Form2.vb
+++ log in/Form2.vb
@@ -1,3 +1,161 @@
 ﻿Public Class Form2
 
 End Class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CLONE 
+1. create a folder in desktop and in CP cd <folder>
+2. Copy repository url in code 
+3. CP, git clone <URL>
+4. Check in folder if files are there 
+
+FORK 
+  1. Go to others Repo 
+2.click on fork 
+3. Create Fork
+
+PERFORMING FIRST COMMIT 
+1. create a new git repository 
+2. in CP Git in
+3. Git commit -m "initial Commit".
+4. Git log
+
+
+PUSH CHANGES 
+1.make changes in cloned local files in note pad 
+2. in CP, CD directed forlder
+3. Git init 
+4. Git remote add origin <URL of the repo>
+5. git add .
+6. git commit -m "message"
+7. git push -u origin master/main
+
+PULL CHANGES 
+1. git init 
+2. Git remote add origin <URL of the repo>
+3. git add .
+4. git commit -m "message"
+5. git pull origin master/main
+6. check text box the changes will be visible 
+
+CREDENTIAL MANAGER 
+1. Windows button 
+2. Search credential manager 
+3. Windows credential manager 
+4. Git and something something
+5. Delete or remore 
+
+CREATING NEW ISSUE 
+1. go to Rohit prem 
+2. Calculator 
+3. Click on issues 
+4. Create new issue 
+5. Provide details 
+6. Mention @
+7. Submit 
+
+CREATING A PULL REQUEST 
+1. Fork 
+2. Clone 
+3. Changes in the local file 
+4. cd and the folder in deep 
+5. git add .
+6. git commit -m "Message"
+7. git push origin 
+8. In our forked repository click on pull request and then new pull request 
+9. create pull request 
+10. give description 
